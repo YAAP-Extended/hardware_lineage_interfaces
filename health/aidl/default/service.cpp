LineageOS/android_hardware_lineage_interfaces

health: Add default implementation
Change-Id: I331689824877c30150c8b8fc2462ab41ab291333

health/aidl/default/service.cpp
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "LineageHealth.h"
+
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <android-base/logging.h>
+
+using ::aidl::vendor::lineage::health::LineageHealth;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<LineageHealth> lh = ndk::SharedRefBase::make<LineageHealth>();
+
+    const std::string instance = std::string() + LineageHealth::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(lh->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}

YAAP-Extended/hardware_lineage_interfaces

health/aidl/default/service.cpp
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2022-2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "ChargingControl.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using ::aidl::vendor::lineage::health::ChargingControl;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<ChargingControl> lh = ndk::SharedRefBase::make<ChargingControl>();
+
+    const std::string instance = std::string() + ChargingControl::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(lh->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}